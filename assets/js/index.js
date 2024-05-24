6dbb4abfdf4a60b34a43c2df709c0b1b9d12c3be
[Chris B] <[baumgart.c@gmail.com]>
2024-05-24 13:05:16 -0600

    renamed files so each page has its own css and js

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+//TODO: Prompt users for sign-in info or to add a new username
+//TODO: Store user login info
+
+
+//TODO: Take username and store.  Attach it to each blog post.
+//TODO: Dynamic screen sizes
+
+
+//TODO:Format blog page with css
+//TODO: Toggle light/dark mode
+//TODO: Use local storage to capture text from blogID to store on blog.html page
+//TODO: Add event listener to "Submit" button to capture and store.
+        
+//TODO:Add time stamp to blog posts to be captured alongside blog elements
+
+//TODO: Bonus: allow users to edit old blog posts.  And timestamp?
