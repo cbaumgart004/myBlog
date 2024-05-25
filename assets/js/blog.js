a46b63ff3993ed682d745a6636eb16128fee7148
[Chris B] <[baumgart.c@gmail.com]>
2024-05-25 11:37:56 -0600

    blogEntries array functional

---
+++ assets/js/blog.js
@@ -0,0 +1,4 @@
+
+    const blogEntries = JSON.parse(localStorage.getItem('blogEntries'));
+    console.log(blogEntries);
+
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,54 @@
 //Set variables for page elements
-const title = document.getElementById('blogTitle')
-const text = document.getElementById('blogText')
-const submitButton = document.getElementsByName('submitButton')
+const title = document.getElementById('blogTitle');
+const text = document.getElementById('blogText');
+const submit = document.getElementById('submitButton');
 //TODO: Save all form data as object using JSON.stringify
-    function saveBlog() {
+//TODO: Use local storage to capture text from blogID to store on blog.html page
+
+function saveBlog() {
         const blogEntry = {
         title: title.value, 
         text: text.value.trim(),
 
     };
-    //Now set it and stringify it
-    localStorage.setItem('blogEntry', JSON.stringify(blogEntry));
+    //Clear fields
+    title.value = '';
+    text.value = '';
+    
+    console.log(blogEntry);
+    addEntry(blogEntry);
+    
+}; 
+//TODO: Add blog entry to blogEntries array
+function addEntry (blogEntry) {
+    //Initialize the blogEntries array ONLY if it doesn't already exist
+    let blogEntries = JSON.parse(window.localStorage.getItem('blogEntries')) || [];
+    console.log(blogEntry);
+    console.log(blogEntries);
+    blogEntries.push(blogEntry);
+    localStorage.setItem('blogEntries', JSON.stringify(blogEntries));
+    console.log(blogEntries);
+
 };
+
 //TODO: Take username and store.  Attach it to each blog post.
 //TODO: Dynamic screen sizes
 
 
 //TODO:Format blog page with css
 //TODO: Toggle light/dark mode
-//TODO: Use local storage to capture text from blogID to store on blog.html page
+
 //TODO: Add event listener to "Submit" button to capture and store.
-        //submitButton.addEventListener('click', )
+        submit.addEventListener('click', function (event) {
+            //don't refresh the page before capturing data
+            event.preventDefault();
+            saveBlog();
+            
+        });
 //TODO:Add time stamp to blog posts to be captured alongside blog elements
 
 //TODO: Bonus: allow users to edit old blog posts.  And timestamp?
 
 //TODO: Prompt users for sign-in info or to add a new username
 //TODO: Store user login info
+//localStorage.clear();
